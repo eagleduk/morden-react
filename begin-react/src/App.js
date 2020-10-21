270aa30d537d232dd51394b32173ecdce754afad
eagleduk <eagleduk@gmail.com>
2020-10-21 22:07:55 +0900

    16. useReducer Hook - useState 처럼 상태를 업데이트시 사용 - 상태 업데이트 로직을 컴포넌트 밖으로 분리 가능 - reducer : 상태를 업데이트하는 함수 - [number,dispatch] = useReducer([함수],[기본값])

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useMemo, useCallback} from 'react';
+import React, {useRef, useState, useMemo, useCallback, useReducer} from 'react';
 
 import "./App.css";
 import UserList from './UserList';
@@ -9,8 +9,12 @@ function countActiveUsers(user) {
   return user.filter(user => user.active).length;
 }
 
-function App() {
-  const [users, setUsers] = useState([
+const initialState = {
+  inputs: {
+    username: "",
+    email: ""
+  },
+  users: [
     {
         id: 1,
         username: "test1",
@@ -29,70 +33,100 @@ function App() {
         email: "test3@email.com",
         active: false
     }
-  ]);
+  ]
+}
 
-  const [inputs, setInputs] = useState({
-    username: "",
-    email: ""
-  })
+function reducer(state, action) {
+  switch(action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map(user => 
+          user.id === action.id
+            ? {...user, active: !user.active}
+            : user
+        )
 
-  const { username, email } = inputs;
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      }
+    default:
+      throw new Error("Unhandled action");
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    
-    //setUsers([...users, user]);
-    setUsers(users => users.concat(user));
+  const { users } = state;
+  const { username, email } = state.inputs;
 
-    setInputs({
-      username: "",
-      email: ""
-    })
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    dispatch({
+      type: "CHANGE_INPUT",
+      name,
+      value
+    });
+  }, []);
+
+  const onCreate = useCallback(e => {
+    dispatch({
+      type: "CREATE_USER",
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
+    });
     nextId.current += 1;
   }, [username, email]);
 
-
-  const onChange = useCallback(e => {
-    const {name,value} = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value
-      })
-  }, [inputs]);
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: "TOGGLE_USER",
+      id
+    });
+  }, []);
 
   const onRemove = useCallback(id => {
-    setUsers(users => users.filter(user => user.id != id));
+    dispatch({
+      type: "REMOVE_USER",
+      id
+    });
   }, []);
 
-  const onToggle = useCallback(id => {
-    setUsers(users =>
-      users.map(
-        user => user.id ===id
-        ? {...user, active: !user.active}
-        : user
-      )
-    );
-  }, []);
-
-  // 두번째 파라메터의 값이 변경될 때 첫번째 파라메터의 함수 수행
-  const count = useMemo(()=>countActiveUsers(users),[users]);
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
       <CreateUser 
-        username={username}
-        email={email}
+        username={username} 
+        email={email} 
         onChange={onChange}
         onCreate={onCreate}
       />
       <UserList 
-        users={users}
-        onRemove={onRemove}
-        onToggle={onToggle}
+        users = {users}
+        onToggle = {onToggle}
+        onRemove = {onRemove}
       />
       <div>
         활성 사용자 수: {count}
