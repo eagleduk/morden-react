dc43c36c1b71d8e06dbbce50ee3a279066b90942
eagleduk <eagleduk@gmail.com>
2020-10-21 22:49:51 +0900

    Custom Hook useState 를 useReducer 로 바꾸기 => 혼자 풀이 성공!!

--- begin-react/src/useInputs.js
+++ begin-react/src/useInputs.js
@@ -1,10 +1,41 @@
-import { useState, useCallback } from "react";
+import { useState, useReducer, useCallback } from "react";
 
-function reducer(state, action) {
-    
+function reducer(form, action) {
+    switch(action.type) {
+        case "CHANGE":
+            console.log(action);
+            return {
+                ...form,
+                [action.name]: action.value
+            }
+        case "RESET":
+            return action.initialForm;
+        default:
+            throw new Error("Unhandled action");
+    }
 }
 
 function useInputs(initialForm) {
+
+    const [form,dispatch] = useReducer(reducer, initialForm);
+
+    const onChange = useCallback(e => {
+        const {name, value} = e.target;
+        dispatch({
+            type: "CHANGE",
+            name,
+            value
+        });
+    });
+
+    const reset = useCallback(e => {
+        dispatch({
+            type: "RESET",
+            initialForm
+        })
+    })
+
+    /*
     const [form, setForm] = useState(initialForm);
 
     const onChange = useCallback(e => {
@@ -13,6 +44,7 @@ function useInputs(initialForm) {
     }, []);
 
     const reset = useCallback(() => setForm(initialForm), [initialForm]);
+    */
 
     return [form,onChange,reset];
 }
