bef01c76187044c98ddb65da2599d57cafab8ef0
eagleduk <eagleduk@gmail.com>
2020-10-22 23:10:28 +0900

    20. class 형 컴포넌트 - 현재는 잘 사용하지 않는 방식 - Hook 를 사용하면서 함수형 컴포넌트를 사용

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -1,38 +1,72 @@
-import React, { useReducer } from "react";
+import React, { Component } from "react";
 
-function reducer(state, action) {
-    switch(action.type) {
-        case "INCREMENT":
-            return state+1;
-        case "DECREMENT":
-            return state-1;
-        default:
-            throw new Error("Unhandled action");
+class Counter extends Component {
+
+    state = {
+        counter: 0,
+        fixed: 1
     }
-}
 
-function Counter() {
+    handleIncrease = () => {
+        // 함수형 업데이트는 바로 반영
+        this.setState(state => ({
+            counter: this.state.counter + 1
+        }));
+    }
 
-    const [number, dispatch] = useReducer(reducer, 0);
-
-    const onIncrease = () => {
-        dispatch({
-            type: "INCREMENT"
+    handleDecrease = () => {
+        // 객체형 업데이트는 바로 반영되지 않는다.
+        this.setState({
+            counter: this.state.counter - 1
         })
     }
 
-    const onDecrease = () => {
-        dispatch({
-            type: "DECREMENT"
-        })
+    render() {
+        return (
+            <div>
+                <h1>{this.state.counter}</h1>
+                <button onClick={this.handleIncrease}>+1</button>
+                <button onClick={this.handleDecrease}>-1</button>
+                <p>고정된 값 {this.state.fixed}</p>
+            </div>
+        );
     }
-    return (
-        <div>
-            <h1>{number}</h1>
-            <button onClick={onIncrease}>+1</button>
-            <button onClick={onDecrease}>-1</button>
-        </div>
-    );
 }
+
+
+// function reducer(state, action) {
+//     switch(action.type) {
+//         case "INCREMENT":
+//             return state+1;
+//         case "DECREMENT":
+//             return state-1;
+//         default:
+//             throw new Error("Unhandled action");
+//     }
+// }
+
+// function Counter() {
+
+//     const [number, dispatch] = useReducer(reducer, 0);
+
+//     const onIncrease = () => {
+//         dispatch({
+//             type: "INCREMENT"
+//         })
+//     }
+
+//     const onDecrease = () => {
+//         dispatch({
+//             type: "DECREMENT"
+//         })
+//     }
+//     return (
+//         <div>
+//             <h1>{number}</h1>
+//             <button onClick={onIncrease}>+1</button>
+//             <button onClick={onDecrease}>-1</button>
+//         </div>
+//     );
+// }
 
 export default Counter;
