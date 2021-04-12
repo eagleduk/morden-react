6dfe5948996cbd2427219fb7e179a088e1a593d3
eagleduk <eagleduk@gmail.com>
2021-04-12 13:02:04 +0900

    error 수정

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Hello from './Hello';
-import Counter from './Counter';
-import User from './User';
-import ErrorBoundary from './ErrorBoundary';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Hello from "./Hello";
+import Counter from "./Counter";
+import User from "./User";
+import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <User />
+      <App />
     </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
